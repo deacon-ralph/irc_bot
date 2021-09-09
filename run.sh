d5f1af5967f22d72587e673ebd88a8477e287bcd
cumdata <cumdata@protonmail.com>
2021-09-09 05:10:41 +0000

    ascii art

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker run --rm \
+-p 6680:6680 \
+-v ~/irc_bot/chatter.log:/irc_bot/chatter.log \
+-v ~/irc_bot/config.toml:/irc_bot/config.toml \
+-v ~/irc_bot/bot_modules:/irc_bot/bot_modules \
+-t "$1"
