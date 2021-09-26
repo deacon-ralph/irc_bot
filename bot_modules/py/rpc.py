5142377b5672c026e4263600895e439674c74897
deacon-ralph <cumdata@protonmail.com>
2021-09-26 06:01:14 +0000

    add file

---
+++ bot_modules/py/rpc.py
@@ -0,0 +1,49 @@
+"""Remote socket plugin"""
+
+import asyncio
+
+import colors
+import logger
+import plugin_api
+
+
+_logger = logger.LOGGER
+
+
+class Plugin(plugin_api.LocalPlugin):
+    """Remote socket handler"""
+
+    client = None
+    stop = False
+    server = None
+
+    def help_msg(self):
+        return "Remote socket plugin"
+
+    def on_reload(self):
+        super().on_reload()
+        self.server.close()
+        print('close called')
+
+    def on_loaded(self, client):
+        super().on_loaded(client)
+        if self.enabled:
+            asyncio.ensure_future(self.socket_listen())
+
+    async def socket_recv(self, reader, writer):
+        data = (await reader.read(255)).decode('utf8')
+        _logger.info(f'got data: {data}')
+        response = 'here is a response!\n'
+        writer.write(response.encode('utf8'))
+        await writer.drain()
+        await writer.wait_closed()
+
+    async def socket_listen(self):
+        self.server = await asyncio.start_server(
+            self.socket_recv, 'localhost',
+            12345
+        )
+        async with self.server:
+            await self.server.serve_forever()
+
+
