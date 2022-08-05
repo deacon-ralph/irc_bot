30844fc31c59bb656225a9bc796051316d3ed792
deacon-ralph <cumdata@protonmail.com>
2022-08-04 21:40:52 -0400

    update run script

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 FOLDER_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker run --rm \
 -p 12345-12444:12345-12444 \
--v $FOLDER_PATH/ascii_art:/irc_bot/ascii_art \
--v $FOLDER_PATH/chatter.log:/irc_bot/chatter.log \
--v $FOLDER_PATH/admins.toml:/irc_bot/admins.toml \
--v $FOLDER_PATH/config.toml:/irc_bot/config.toml \
--v $FOLDER_PATH/bot_modules:/irc_bot/bot_modules \
+-v "$FOLDER_PATH"/ascii_art:/irc_bot/ascii_art \
+-v "$FOLDER_PATH"/chatter.log:/irc_bot/chatter.log \
+-v "$FOLDER_PATH"/admins.toml:/irc_bot/admins.toml \
+-v "$FOLDER_PATH"/config.toml:/irc_bot/config.toml \
+-v "$FOLDER_PATH"/bot_modules:/irc_bot/bot_modules \
 -t "$1"
