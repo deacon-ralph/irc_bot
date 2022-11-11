a86f1f83fbe3ca3a2f29fdaa19ddd360c274a54f
deacon-ralph <cumdata@protonmail.com>
2022-11-10 22:57:25 -0500

    fix? idk

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pydle
 import pydle.client as pydle_client
 import pydle.features.rfc1459.protocol as protocol
+import pydle.features.isupport
 
 import bots
 import common
@@ -53,10 +54,47 @@ def _patched_create_user(self, nickname):
     }
 
 
+async def _patch_on_raw_005(self, message):
+    """ patched ISUPPORT indication. """
+
+    FEATURE_DISABLED_PREFIX = '-'
+    isupport = {}
+
+    # Parse response.
+    # Strip target (first argument) and 'are supported by this server' (last argument).
+    for feature in message.params[1:-1]:
+        if feature.startswith(FEATURE_DISABLED_PREFIX):
+            value = False
+        elif '=' in feature:
+            feature, value = feature.split('=', 1)
+        else:
+            value = True
+        isupport[feature.upper()] = value
+
+    # Update internal dict first.
+    self._isupport.update(isupport)
+
+    # And have callbacks update other internals.
+    for entry, value in isupport.items():
+        if value is not False:
+            # A value of True technically means there was no value supplied; correct this for callbacks.
+            if value is True:
+                value = None
+
+            method = 'on_isupport_' + pydle.protocol.identifierify(entry)
+            if hasattr(self, method):
+                try:
+                    await getattr(self, method)(value)
+                except TypeError:
+                    print('shit went south?')
+
+
 # patch rfc1459 parsing
 pydle.features.rfc1459.parsing.parse_user = _patched_parse_user
 # patch for pdyle.client.BaseClient
 pydle_client.BasicClient._create_user = _patched_create_user
+#patch for isupport 005 raw handler
+pydle.features.isupport.on_raw_005 = _patch_on_raw_005
 
 
 def _make_client(chatnet, data):
