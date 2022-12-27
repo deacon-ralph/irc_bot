51696a124d04522e1a788cf6300e84a1e6a59608
deacon-ralph <cumdata@protonmail.com>
2022-12-26 22:25:03 -0500

    update service module

--- bot_modules/py/ghetto_service.py
+++ bot_modules/py/ghetto_service.py
@@ -20,18 +20,13 @@ _logger = logger.LOGGER
 class Plugin(plugin_api.LocalPlugin):
     """Ghetto service plugin"""
 
-    @property
-    def enabled(self):
-        """ALWAYS ENABLED!"""
-        return True
-
     def help_msg(self):
         return {
             'HUH': 'whats going on',
             'defcon': 'set the defcon level 1-4'
         }
 
-    async def _defcon_1(self, target):
+    async def _defcon_4(self, target):
         """Sets the channel to +m so only voiced users can chat
 
         :param str target: the channel
@@ -46,7 +41,7 @@ class Plugin(plugin_api.LocalPlugin):
             ' '.join(self.client.channels[target]['users'])
         )
 
-    async def _defcon_2(self, target):
+    async def _defcon_3(self, target):
         """Sets the channel to invite only and only voiced users can chat
 
         :param str target: the channel
@@ -61,7 +56,7 @@ class Plugin(plugin_api.LocalPlugin):
             ' '.join(self.client.channels[target]['users'])
         )
 
-    async def _defcon_3(self, target):
+    async def _defcon_2(self, target):
         """Sets channel to invite, only voiced users, and devoices users
 
         :param str target: the channel
@@ -78,7 +73,7 @@ class Plugin(plugin_api.LocalPlugin):
             ' '.join(self.client.channels[target]['users'])
         )
 
-    async def _defcon_4(self, target):
+    async def _defcon_1(self, target):
         """Sets channel to invite only, devoices voiced users, and kicks
 
         :param str target: the channel
@@ -141,7 +136,7 @@ class Plugin(plugin_api.LocalPlugin):
     async def on_kick(self, channel, target, by, reason=None):
         await super().on_kick(channel, target, by, reason)
         if await common.is_user_admin(self.client, target):
-            await self._defcon_2(channel)
+            await self._defcon_3(channel)
             # make sure we dont kick a bot admin who kicked another admin
             if not await common.is_user_admin(self.client, by):
                 # wasnt a bot admin, give em the boot
@@ -178,7 +173,7 @@ class Plugin(plugin_api.LocalPlugin):
         for user in affected_users:
             if await common.is_user_admin(self.client, user):
                 if not any(user in mode for mode in [o, a, q]):
-                    await self._defcon_2(channel)
+                    await self._defcon_3(channel)
                     await self._kick_all_with_same_host(channel, by)
 
     async def _kick_all_with_same_host(self, channel, target):
