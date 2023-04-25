b4ef5115d49a8605ff94a2be6274a85af6e981be
Максим <cumdata@protonmail.com>
2023-04-25 18:26:37 +0000

    use since ids

--- bot_modules/py/fishtank.py
+++ bot_modules/py/fishtank.py
@@ -1,4 +1,5 @@
 import asyncio
+import dataclasses
 
 import pendulum
 import tweepy
@@ -12,13 +13,20 @@ import plugin_api
 _logger = logger.LOGGER
 
 
+@dataclasses.dataclass
+class _UserQuery:
+    username : str
+    since_id: int
+
+
 class Plugin(plugin_api.LocalPlugin):
     """Plugin to send fishtankdotlive tweets to irc"""
 
     def __init__(self):
         super().__init__()
         self.tweet_scraper_task = None
-        self.last_scraped = pendulum.now(tz='UTC')
+        self.last_ftdl_id = _UserQuery('fishtankdotlive', None)
+        self.last_cfd_id = _UserQuery('DoctorCumFart', None)
         self.twitter_config = common.parse_config().get('twitter')
         self.bearer_token = self.twitter_config.get('bearer_token')
         self.api_key = self.twitter_config.get('api_key')
@@ -41,23 +49,31 @@ class Plugin(plugin_api.LocalPlugin):
         super().on_reload()
         self.tweet_scraper_task.cancel()
 
-    def _get_tweets(self, api, username):
+    def _get_tweets(self, api, username_query):
         """Get tweets for username
 
         :param tweepy.API api: twitter api
-        :param str username: twitter username
+        :param _UserQuery username_query: twitter username
 
         :returns: list of status
         :rtype: list
         """
+        if username_query.since_id is None:
+            count = 1
+        else:
+            count = 20
         tweets = api.user_timeline(
-            screen_name=username,
-            count=20,
+            screen_name=username_query.username,
+            since_id=username_query.since_id,
+            count=count,
             exclude_replies=False,
             tweet_mode='extended'
         )
         tweets.reverse()
-        _logger.info(f'Found {len(tweets)} tweets for {username}')
+        username_query.since_id = tweets[-1].id
+        _logger.info(
+            f'Found {len(tweets)} tweets for {username_query.username}'
+        )
         return tweets
 
     async def _read_tweets(self):
@@ -66,21 +82,15 @@ class Plugin(plugin_api.LocalPlugin):
             auth = tweepy.OAuthHandler(self.api_key, self.api_key_secret)
             auth.set_access_token(self.access_token, self.access_token_secret)
             api = tweepy.API(auth, wait_on_rate_limit=True)
-            usernames = ['fishtankdotlive', 'DoctorCumFart']
-            for username in usernames:
-                _logger.info(f'fetching tweets from {username}')
-                tweets = self._get_tweets(api, username)
+            username_queries = [self.last_ftdl_id, self.last_cfd_id]
+            for username_query in username_queries:
+                _logger.info(f'fetching tweets from {username_query.username}')
+                tweets = self._get_tweets(api, username_query)
                 for tweet in tweets:
-                    _logger.info(f'last_scraped={self.last_scraped}, tweet.created_at={tweet.created_at}, should show: {tweet.created_at > self.last_scraped}')
-                    if tweet.created_at > self.last_scraped:
-                        _logger.info(
-                            f'found tweet newer then '
-                            f'{self.last_scraped.isoformat()}'
-                        )
-                        text = tweet.full_text
-                        await self.client.message(
-                            '#fishtanklive',
-                            f'{colors.colorize(text="🐠 @"+ username, fg=colors.WHITE, bg=colors.BLUE_TWITTER)} {text}'
-                        )
+                    username = username_query.username
+                    text = tweet.full_text
+                    await self.client.message(
+                        '#fishtanklive',
+                        f'{colors.colorize(text="🐠 @"+ username, fg=colors.WHITE, bg=colors.BLUE_TWITTER)} {text}'
+                    )
                 await asyncio.sleep(320)
-            self.last_scraped = pendulum.now(tz='UTC')
